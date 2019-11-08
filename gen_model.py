aaf60452465144b783c549126ec4b7d66184a257
Ben Walsh <bwalsh62@gmail.com>
2019-11-07 21:54:01 -0600

    Differentiating between 7 note classes. Adding music dictionary to make_tone and importing

--- gen_model.py
+++ gen_model.py
@@ -4,14 +4,13 @@
 Train ML to recognize notes from input sound
 
 Created on Sun Nov  3 18:12:29 2019
-Last updated November 5 2019
+Last updated November 7 2019
 
 @author: Ben Walsh
 
 TO DO
 - Extract FFT features
-- Try 4 notes/classes
-- import music_dict from another file
+- Try a more advanced algorithm
 
 """
 
@@ -26,93 +25,57 @@ from sklearn import tree
 from sklearn.metrics import accuracy_score
 
 # Custom library to make tones
-from make_tone import tone
+from make_tone import tone, music_dict
 from noise_util import add_noise
 
 #%% Generate pure sine waves
 
-mus_dict = {
-        'D2': 73.42,
-        'E2': 82.41,
-        'F2': 87.31,
-        'G2': 98.00,
-        'A2': 110.00,
-        'B2': 123.47,
-        'C3': 130.81,
-        'D3': 146.83,
-        'E3': 164.81,
-        'F3': 174.61,
-        'G3': 196.00,
-        'A3': 220.00,
-        'B3': 246.94,
-        'C4': 261.63,
-        'D4': 293.66,
-        'E4': 329.63,
-        'F4': 349.23,
-        'G4': 392.00,
-        'A4': 440.00,
-        'B4': 493.88,
-        'C5': 523.25,
-        'D5': 587.33,
-        'E5': 659.25,
-        'F5': 698.46,
-        'G5': 783.99
-}
-
 t_len = 0.1 # seconds
-notes = ['C4','E4','G4']
-f1 = mus_dict[notes[0]] 
-tone1 = tone(f1,t_len)
-
-f2 = mus_dict[notes[1]]
-tone2 = tone(f2,t_len)
-
-f3 = mus_dict[notes[2]]
-tone3 = tone(f3,t_len)
-
-tone_classes = [tone1.sin_wave, tone2.sin_wave, tone3.sin_wave]
+notes = ['C4','D4','E4','F4','G4','A4','B4']
+freqs = [music_dict[note] for note in notes]
+tones = [tone(f,t_len) for f in freqs]
 
 #%% Plot waveform
 
 # Create array of time samples
-t_array_len = tone1.time_len*tone1.fs
+t_array_len = tones[0].time_len*tones[0].fs
 t_array = np.arange(t_array_len)
 
 # Plot waveform over short time period to see sine
 plt.subplot(121)
-plt.plot(t_array/tone1.fs,tone1.sin_wave)
+plt.plot(t_array/tones[0].fs,tones[0].sin_wave)
 plt.xlim(0 ,0.05)
 plt.xlabel('Time (s)')
-plt.title('Sine wave at ' + str(tone1.f0) + ' hz')
+plt.title('Sine wave at ' + str(tones[0].f0) + ' hz')
 
 # Plot waveform over short time period to see sine
 plt.subplot(122)
-plt.plot(t_array/tone2.fs,tone2.sin_wave)
+plt.plot(t_array/tones[1].fs,tones[1].sin_wave)
 plt.show
 plt.xlim(0 ,0.05)
 plt.xlabel('Time (s)')
-plt.title('Sine wave at ' + str(tone2.f0) + ' hz')
+plt.title('Sine wave at ' + str(tones[1].f0) + ' hz')
 
 #%% Create dataset by adding random shifts and noise
 
 # Declare number of entries
-n_entries = 100
-n_class = len(tone_classes)
+n_entries = 200
+n_class = len(tones)
 
 # Initialize matrix where each row contains a sine wave
-X = np.empty((n_entries*n_class,len(tone1.sin_wave)))
+X = np.empty((n_entries*n_class,len(tones[0].sin_wave)))
 
 # Add white noise to each element
-for idx, tone_class in enumerate(tone_classes):
+for idx, tone_class in enumerate(tones):
     for sample in range(n_entries):
-        X[sample+n_entries*(idx),:] = add_noise(tone_class)
+        X[sample+n_entries*(idx),:] = add_noise(tone_class.sin_wave)
     
 #%% Plot example with added noise
-plt.plot(t_array/tone1.fs,X[0,:])
+plt.plot(t_array/tones[0].fs,X[0,:])
 plt.show
 plt.xlim(0 ,0.05)
 plt.xlabel('Time (s)')
-plt.title('Sine wave at ' + str(tone1.f0) + ' hz with noise')
+plt.title('Sine wave at ' + str(tones[0].f0) + ' hz with noise')
 
 ##%% Create features by taking FFT and extracting power at each note
 ## Skip for now
@@ -135,18 +98,13 @@ plt.title('Sine wave at ' + str(tone1.f0) + ' hz with noise')
 #%% Create training dataset
 
 # For simplicity for now just take first 100 samples as input features
-X = X[:,:100]
+X = X[:,:200]
 
 # Training truth labels
 y = np.empty((n_entries*n_class,1))
 
-#for idx, note in enumerate(notes):
-#    y[n_entries*(idx):n_entries*(idx+1)]=note
-
-y[:n_entries] = '1'#f1
-y[n_entries:n_entries*2] = '2'#f2
-y[n_entries*2:n_entries*3] = '3'#f3
-
+for idx, note in enumerate(notes):
+    y[n_entries*(idx):n_entries*(idx+1)]=str(idx)
 
 #%% Implement ML model
 
@@ -154,8 +112,8 @@ y[n_entries*2:n_entries*3] = '3'#f3
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
 # Implement Decision Tree Model
-model = tree.DecisionTreeClassifier(max_depth=3,
-            max_features=3, max_leaf_nodes=3, random_state=1)
+model = tree.DecisionTreeClassifier(max_depth=10,
+            max_features=10, max_leaf_nodes=10, random_state=1)
 # Fit model to training set
 model.fit(X_train, y_train)
 
@@ -163,4 +121,4 @@ model.fit(X_train, y_train)
 y_predict = model.predict(X_test)
 
 # See accuracy on test set
-accuracy_score(y_test, y_predict)
+print("Accuracy on test set "+str(100*accuracy_score(y_test, y_predict)))
