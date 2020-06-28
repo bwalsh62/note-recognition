b1f3a6f15bae146b67257d3dfdd1dd0cba4dfa38
Ben Walsh <bwalsh62@gmail.com>
2020-06-27 19:30:03 -0500

    Adding model testing into notebook and fixing relative paths

--- piano_notes.py
+++ piano_notes.py
@@ -16,25 +16,33 @@ Last updated: November 13, 2019
 
 #%% Import libraries
 from pygame import mixer
-from make_tone import music_dict
+import os
+import sys
+
+# Add custom modules to path
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from util.music_util import freq_dict
 
 #%%
 mixer.init()
 
 # Paths to piano wav files
-music_fpath = "../piano-gui/music_files/piano/"
-C_path = music_fpath+"Piano.mf.C4_2p4s.wav"
-Csharp_path = music_fpath+"Piano.mf.Db4_2p5s.wav"
-D_path = music_fpath+"Piano.mf.D4_2p4s.wav"
-Dsharp_path = music_fpath+"Piano.mf.Eb4_2p5s.wav"
-E_path = music_fpath+"Piano.mf.E4_2p4s.wav"
-F_path = music_fpath+"Piano.mf.F4_2p4s.wav"
-Fsharp_path = music_fpath+"Piano.mf.Gb4_2p5s.wav"
-G_path = music_fpath+"Piano.mf.G4_2p4s.wav"
-Gsharp_path = music_fpath+"Piano.mf.Ab4_2p5s.wav"
-A_path = music_fpath+"Piano.mf.A4_2p4s.wav"
-Asharp_path = music_fpath+"Piano.mf.Bb4_2p5s.wav"
-B_path = music_fpath+"Piano.mf.B4_2p4s.wav"
+music_fpath = r"..\..\piano-gui\music_files\piano"
+C_path = os.path.join(music_fpath,"Piano.mf.C4_2p4s.wav")
+Csharp_path = os.path.join(music_fpath,"Piano.mf.Db4_2p5s.wav")
+D_path = os.path.join(music_fpath,"Piano.mf.D4_2p4s.wav")
+Dsharp_path = os.path.join(music_fpath,"Piano.mf.Eb4_2p5s.wav")
+E_path = os.path.join(music_fpath,"Piano.mf.E4_2p4s.wav")
+F_path = os.path.join(music_fpath,"Piano.mf.F4_2p4s.wav")
+Fsharp_path = os.path.join(music_fpath,"Piano.mf.Gb4_2p5s.wav")
+G_path = os.path.join(music_fpath,"Piano.mf.G4_2p4s.wav")
+Gsharp_path = os.path.join(music_fpath,"Piano.mf.Ab4_2p5s.wav")
+A_path = os.path.join(music_fpath,"Piano.mf.A4_2p4s.wav")
+Asharp_path = os.path.join(music_fpath,"Piano.mf.Bb4_2p5s.wav")
+B_path = os.path.join(music_fpath,"Piano.mf.B4_2p4s.wav")
 
 # Define sounds
 sound_C = mixer.Sound(C_path)
@@ -74,6 +82,6 @@ class note_class:
     def __init__(self, note, instr='piano'):        
         #self.f0 = f0 # frequency in Hz
         self.note = note # Example C4
-        self.f0 = music_dict[note]
+        self.f0 = freq_dict[note]
         self.instr = instr
         self.sound = sound_dict[note]
